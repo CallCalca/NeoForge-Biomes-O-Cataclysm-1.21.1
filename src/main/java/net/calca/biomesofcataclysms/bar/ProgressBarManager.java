fa9967a252c6ffaa19755c6ad3573e794b2ce7dc
MATTIU234609 <matteo.panighel2007@gmail.com>
2026-05-24 20:52:23 +0200

    version 1.3.0 --- NEW FEATURES --- 1) Added translations for new languages.    Currently supported languages are: American English, German, Spanish, Mexican Spanish, Italian, Japanese, Chinese, and Venetian dialect. ---

--- src/main/java/net/calca/biomesofcataclysms/bar/ProgressBarManager.java
+++ src/main/java/net/calca/biomesofcataclysms/bar/ProgressBarManager.java
@@ -1,16 +1,11 @@
 package net.calca.biomesofcataclysms.bar;
 
-import com.sun.jna.platform.win32.Variant;
 import net.calca.biomesofcataclysms.data.ModVariables;
-import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.BossEvent;
-import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.player.Player;
-
-import java.awt.*;
 
 public class ProgressBarManager {
     public static void initializeOnServerStart(ModVariables.MapVariables variables, ServerLevel serverLevel){
@@ -48,8 +43,7 @@ public class ProgressBarManager {
             setOverlay(minutes);
             TIMER_PROGRESS_BAR.startForAllPlayers(serverLevel);
             setColor(variables);
-            if (variables.timer == -1) TIMER_PROGRESS_BAR.getBossBar().setVisible(false);
-            else TIMER_PROGRESS_BAR.getBossBar().setVisible(true);
+            TIMER_PROGRESS_BAR.getBossBar().setVisible(variables.timer != -1);
         }
 
         public static void setOverlay(int minutes){
@@ -96,6 +90,81 @@ public class ProgressBarManager {
         public static void tick(ModVariables.MapVariables variables){
             setTitle(variables);
             TIMER_PROGRESS_BAR.setRemainingTicks(variables.timer);
+        }
+
+    }
+    public static class SunStormProgressBar {
+        public static final ProgressBar SUN_STORM_PROGRESS_BAR =
+                new ProgressBar(
+                10,
+                BossEvent.BossBarColor.YELLOW,
+                Component.translatable("progressBar.title.sunStormProgressBar").getString()
+        );
+
+        public static void initialize(ServerLevel serverLevel){
+            ModVariables.MapVariables variables = ModVariables.MapVariables.get(serverLevel);
+            initialize(variables, serverLevel);
+        }
+
+        public static void initialize(ModVariables.MapVariables variables, ServerLevel serverLevel){
+            int maxValue = (variables.totalBiomes) * variables.tickDelayBetweenCataclysm;
+            SUN_STORM_PROGRESS_BAR.setMaxValue(maxValue);
+            System.out.println("MAX:" + maxValue/20/60);
+            int totalSeconds = variables.timer / 20;
+            int minutes = totalSeconds / 60;
+            int seconds = totalSeconds % 60;
+            String formattedTime = String.format("%02d:%02d", minutes, seconds);
+            SUN_STORM_PROGRESS_BAR.setTitle(Component.translatable("progressBar.title.timerProgressBar", formattedTime));
+            setOverlay(minutes);
+            SUN_STORM_PROGRESS_BAR.startForAllPlayers(serverLevel);
+            setColor(variables);
+            SUN_STORM_PROGRESS_BAR.getBossBar().setVisible(variables.timer != -1);
+        }
+
+        public static void setOverlay(int minutes){
+            if (minutes <= 63) SUN_STORM_PROGRESS_BAR.setOverlay(BossEvent.BossBarOverlay.NOTCHED_6);
+            else if (minutes < 120) SUN_STORM_PROGRESS_BAR.setOverlay(BossEvent.BossBarOverlay.NOTCHED_10);
+            else if (minutes < 240) SUN_STORM_PROGRESS_BAR.setOverlay(BossEvent.BossBarOverlay.NOTCHED_12);
+            else SUN_STORM_PROGRESS_BAR.setOverlay(BossEvent.BossBarOverlay.NOTCHED_20);
+
+        }
+
+        public static void setColor(ModVariables.MapVariables variables){
+            if (variables.biomesAffected == variables.totalBiomes || (variables.timer <= 0 && variables.timer != -1)){
+                SUN_STORM_PROGRESS_BAR.setColor(BossEvent.BossBarColor.RED);
+                SUN_STORM_PROGRESS_BAR.setProgress(0.0f);
+            }else if (variables.gracePeriod > 0 && variables.timer != -1){
+                SUN_STORM_PROGRESS_BAR.setColor(BossEvent.BossBarColor.YELLOW);
+            }else{
+                SUN_STORM_PROGRESS_BAR.setColor(BossEvent.BossBarColor.BLUE);
+            }
+        }
+
+        public static void setTitle(ModVariables.MapVariables variables){
+            int totalSeconds = variables.timer / 20;
+            int minutes = totalSeconds / 60;
+            int seconds = totalSeconds % 60;
+            String formattedTime;
+            if (variables.difficulty <= 1){
+                formattedTime = String.format("%02d:%02d", minutes, seconds);
+            } else if (variables.difficulty > 3) {
+                String obfuscated = "§k01";
+                formattedTime = String.valueOf(obfuscated + "§r:" + obfuscated);
+            }else{
+                String obfuscated = "§k01";
+                formattedTime = String.valueOf(String.format("%02d", minutes) + ":" + obfuscated);
+            }
+            SUN_STORM_PROGRESS_BAR.setTitle(Component.translatable("progressBar.title.timerProgressBar", formattedTime));
+
+        }
+
+        public static void tick(ServerLevel serverLevel){
+            ModVariables.MapVariables variables = ModVariables.MapVariables.get(serverLevel);
+            tick(variables);
+        }
+        public static void tick(ModVariables.MapVariables variables){
+            setTitle(variables);
+            SUN_STORM_PROGRESS_BAR.setRemainingTicks(variables.timer);
         }
 
     }
